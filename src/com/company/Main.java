f078e9f0a20b52457e98e5f0247e55a8dbef117e
Laila Duffy <lail.duffy@gmail.com>
2021-08-28 14:43:55 +0100

    Finished the second part of this task using the Hashtable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,13 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // HashMap task
         HashMap<String, Double> productInfo = new HashMap<String, Double>();
 
         productInfo.put("Audi", 0.5);
@@ -57,6 +60,61 @@ public class Main {
         Collections.sort(productPricesSorted);
 
         System.out.println("Prices in ascending order: " + productPricesSorted);
+        System.out.println();
+
+        // HashTable task
+        Hashtable<String, Double> productList = new Hashtable<>();
+
+        productList.put("Audi", 0.5);
+        productList.put("BMW", 0.25);
+        productList.put("Mazda", 1.0);
+        productList.put("Volvo", 3.5);
+        productList.put("Tesla", 3.3);
+        productList.put("Lada", 1.1);
+        productList.put("VW", 1.2);
+        productList.put("Saab", 1.0);
+
+        counter = 0;
+        for (Double price : productList.values()) {
+            if (price <= 1.0) {
+                counter++;
+            }
+        }
+        System.out.println("There are " + counter + " items 1EUR or less.");
+
+        minPrice = productList.get("Audi");
+        for (Double price : productList.values()) {
+            if (price <= minPrice) {
+                minPrice = price;
+            }
+        }
+        System.out.println("The cheapest price is: " + minPrice);
+
+        maxPrice = productList.get("Audi");
+        for (Double price : productList.values()) {
+            if (price >= maxPrice) {
+                maxPrice = price;
+            }
+        }
+        System.out.println("The most expensive price is: " + maxPrice);
+
+        HashMap<String, Double> anotherList = new HashMap<>();
+
+        anotherList.put("Audi", 3.7);
+        anotherList.put("Opel", 1.5);
+        anotherList.put("Renault", 4.4);
+
+        anotherList.putAll(productList);
+        System.out.println("Total amount of products: " + anotherList.size());
+
+        ArrayList<Double> productsSorted = new ArrayList<>();
+
+        productsSorted.addAll(anotherList.values());
+        Collections.sort(productsSorted);
+
+        System.out.println("Prices in ascending order: " + productsSorted);
+
+
 
 
     }
